d578080f05c2735007cca2e91ad32d9be4657ddb
EmpIreR1993 <Kirill.tanatos1993@mail.ru>
2024-11-05 11:15:43 +0300

    fix

--- blog_cbv/settings.py
+++ blog_cbv/settings.py
@@ -78,9 +78,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'blog_cbv.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
